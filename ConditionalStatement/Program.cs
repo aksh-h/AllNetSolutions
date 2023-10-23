fd22154baeafdcb4a7214e1d5aa6901fb27c5eab
aksh-h <h.akshay@outlook.com>
2023-10-23 18:31:53 +0530

    added if else

---
+++ ConditionalStatement/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// if statement
+
+
+Console.WriteLine("Enter a number between 0 and 9");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = 0;
+while (x > 0)
+{
+    if (x! < 9)
+    {
+        y = x switch
+        {
+            // till 10
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 4,
+            4 => 5,
+            5 => 6,
+            6 => 7,
+            7 => 8,
+            8 => 9,
+            9 => 10,
+            _ => 0
+        };
+
+        Console.WriteLine($"The value of y is {y}");
+        Console.WriteLine("Enter another number between 0 and 9");
+        x = Convert.ToInt32(Console.ReadLine());
+    }
+    else
+    {
+        Console.WriteLine("The number is greater than 9");
+        break;
+    }
+}
